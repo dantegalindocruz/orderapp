1f6aa81c0073d7ed35a8f6eeaccd2b25f169e72c
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-04 20:55:14 -0400

    add functionality to add and delete items from app

--- js/order.js
+++ js/order.js
@@ -4,18 +4,57 @@ var nextButton = document.querySelector('input[type="submit"]');
 var productForm = document.querySelectorAll('input[type="text"]');
 var productLabel = document.querySelectorAll('label');
 
-addItem.addEventListener('click', function(e){
-    nextButton.style.display= "none";
-    addItem.style.display="none";
-    form.innerHTML +=`
-    <label>Product Name</label>
-    <input type="text" name="product-name">
-    <label>Quantity</label>
-    <input type="text" name="quantity">
-    <label>link</label>
-    <input type="text" name="link">
-    <a href="#"><label class="add-item">Add item +</label></a>
-    <input type="submit" value="Next">
-    `
+function newItem(){
+  // var hr = document.createElement('hr');
+  // form.appendChild(hr);
 
-})
+  let container = document.createElement('div');
+  container.className = "container";
+  for(var i =2; i < productForm.length; i++){
+
+    var input = document.createElement('input');
+    var label = document.createElement('label');
+    var labelName = document.createTextNode(productLabel[i].innerText);
+    input.type = productForm[i].type;
+    input.name = productForm[i].name;
+    label.appendChild(labelName);
+    container.appendChild(label);
+    container.appendChild(input);
+  }
+  form.appendChild(container);
+   deleteButton();
+
+}
+
+function addButtons(){
+  form.appendChild(addItem);
+  form.appendChild(nextButton);
+}
+
+function removeButtons(){
+  form.removeChild(addItem);
+  form.removeChild(nextButton);
+}
+
+function deleteButton(){
+  var button = document.createElement('a');
+  text = document.createTextNode('delete');
+  button.appendChild(text);
+  button.href="#";
+  button.className = 'delete-item';
+  form.appendChild(button);
+}
+addItem.addEventListener('click', function(e){
+    removeButtons();
+    newItem();
+    addButtons();
+
+    var deleteButton = document.querySelector('.delete-item');
+    let container = document.querySelector('.container');
+
+    deleteButton.addEventListener('click', function(e){
+      form.removeChild(deleteButton);
+      form.removeChild(container);
+    })
+
+});
